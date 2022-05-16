a2bb6d57afb8d426e9a669d6789bc8069578a576
trapvpack <vitost666@gmail.com>
2022-05-16 13:35:45 +0300

    changed logo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          i hate all
+          i hate all+
+          hjhighgjh
         </p>
         <a
           className="App-link"
